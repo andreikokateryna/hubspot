60331a6ef64f437cd40c899c179f1f630815f230
andreikokateryna <andreikokateryna.a@gmail.com>
2025-06-17 20:14:03 +0200

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-mkdir hubspot-practicum
-cd hubspot-practicum
-npm init -y
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Головна
+app.get('/', (req, res) => {
+  res.render('index', { title: 'HubSpot Integration', message: 'Hello from HubSpot Integration!' });
+});
+
+// Список кастомних обʼєктів
+app.get('/custom-objects', async (req, res) => {
+  try {
+    const url = `https://api.hubapi.com/crm/v3/objects/${process.env.CUSTOM_OBJECT}`;
+    const response = await axios.get(url, {
+      headers: { Authorization: `Bearer ${process.env.HUBSPOT_TOKEN}` }
+    });
+    res.render('custom-objects', { objects: response.data.results });
+  } catch (error) {
+    res.status(500).send('Error fetching custom objects');
+  }
+});
+
+// Деталі обʼєкта
+app.get('/custom-objects/:id', async (req, res) => {
+  try {
+    const url = `https://api.hubapi.com/crm/v3/objects/${process.env.CUSTOM_OBJECT}/${req.params.id}`;
+    const response = await axios.get(url, {
+      headers: { Authorization: `Bearer ${process.env.HUBSPOT_TOKEN}` }
+    });
+    res.render('custom-object', { object: response.data });
+  } catch (error) {
+    res.status(500).send('Error fetching object');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT
